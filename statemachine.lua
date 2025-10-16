9c8b054353f79e6ab1d76f96ebfbad6977708eda
t0suj4 <valsiterb@gmail.com>
2025-10-17 01:43:39 +0200

    cleanup + add LuaDoc

--- statemachine.lua
+++ statemachine.lua
@@ -1,3 +1,9 @@
+--- Finite State Machine.
+-- A port of `javascript-state-machine` by `jakesgordon`
+-- https://github.com/jakesgordon/javascript-state-machine
+-- @module statemachine
+-- @alias machine
+
 -- https://github.com/kyleconroy/lua-state-machine
 
 -- Copyright (c) 2012 Kyle Conroy
@@ -22,23 +28,38 @@
 -- SOFTWARE.
 
 
-local serpent = require("serpent")
 
+---@class Fsm
 local machine = {}
 machine.__index = machine
 
+---@alias None "none"
+---@alias Async "async"
+
+--- A constant representing no active asynchronous operation.
+--- @type None
 local NONE = "none"
+
+--- A constant to be returned from a callback to indicate an asynchronous operation.
+--- This will pause the transition until `fsm:transition()` is called again.
+--- @type Async
 local ASYNC = "async"
 
 -- Compatibility for Lua 5.1 vs 5.2+
 local unpack = unpack or table.unpack
 
-local function call_handler(handler, machine, params)
+---@param handler EventCallback
+---@param m Fsm
+---@param params AnySerializableType[]
+---@return Async|nil
+local function call_handler(handler, m, params)
     if handler then
-        return handler(machine, unpack(params))
+        return handler(m, unpack(params))
     end
 end
 
+---@param name string Event name
+---@return fun(self: Fsm, ...: AnySerializableType)
 local function create_transition(name)
     local function transition(self, ...)
         local state = self._state
@@ -99,22 +120,22 @@ local function create_transition(name)
     return transition
 end
 
-local allStates, haveWildcard
+--- @type table<string, boolean>
+local allStates
 
-function add_handler(t, info, handler, overwrite)
-    local name = info
-    if t[name] ~= nil and not overwrite then
-        error("Attempted to overwrite event handler: " .. name)
-    end
-    rawset(t, name, handler)
-end
+--- @type boolean
+local haveWildcard
 
+--- @param t table<string, string>
+--- @param name string
 function add_event_handler_names(t, name)
     t["onbefore" .. name] = "onbefore" .. name
     t["onafter" .. name] = "onafter" .. name
     t["on" .. name] = "onafter" .. name
 end
 
+--- @param t table<string, string>
+--- @param from string
 function add_from_handler_names(t, from)
     if from == "*" then
         haveWildcard = true
@@ -124,12 +145,15 @@ function add_from_handler_names(t, from)
     end
 end
 
+--- @param t table<string, string>
+--- @param to string
 function add_to_handler_names(t, to)
     allStates[to] = true
     t["onenter" .. to] = "onenter" .. to
     t["on" .. to] = "onenter" .. to
 end
 
+--- @param t table<string, string>
 function settle_wildcards(t)
     if haveWildcard then
         for k in pairs(allStates) do
@@ -138,6 +162,9 @@ function settle_wildcards(t)
     end
 end
 
+--- @param map table<string, string>
+--- @param callbacknames table<string, string>
+--- @param event EventDefinition
 local function add_to_map(map, callbacknames, event)
     if type(event.from) == 'string' then
         map[event.from] = event.to
@@ -151,12 +178,13 @@ local function add_to_map(map, callbacknames, event)
     add_to_handler_names(callbacknames, event.to)
 end
 
+--- @param v any
 function is_callable_or_nil(v)
     local t = type(v)
     if t == "function" or t == "nil" then
         return true, t
     elseif t == "table" then
-        mt = getmetatable(v)
+        local mt = getmetatable(v)
         if type(mt.__call) == "function" then
             return true, t
         end
@@ -184,6 +212,47 @@ function machine.__newindex(t, k, v)
     end
 end
 
+---@class EventDefinition
+---@field name string Event name
+---@field from string|string[] States the event transitions from or "*" for any state
+---@field to string State the event transitions to
+local _
+
+---@alias SerializableTable table
+---@alias SerializableUserData userdata
+---@alias AnySerializableType SerializableTable|SerializableUserData|string|number|boolean|nil
+---@alias EventCallback fun(fsm: Fsm, event: string, from: string, to: string, ...: AnySerializableType): Async|void
+---@alias EventCallbacks table<string, EventCallback>
+
+---@class FsmOptions
+---@field public initial string|nil default "none", Initial state
+---@field public events EventDefinition[] Valid states and events of the machine
+---@field public callbacks EventCallbacks State transition callbacks
+---@field public state table|nil default {}, Table to hold the machine state
+---@field public lax boolean|nil default false, True will allow storing data on the machine object and replacing callbacks
+---@field public seal boolean|nil default false, True will prevent any modifications
+local _
+
+---@alias EventTriggers table<string, fun(self: self, ...: AnySerializableType)>
+
+--- Creates a new state machine.
+--- @param options FsmOptions The options for the state machine.
+--- @return Fsm|EventCallbacks|EventTriggers The new state machine object.
+--- @usage
+--- local fsm = statemachine.create{
+---   initial = 'green',
+---   events = {
+---     { name = 'warn', from = 'green', to = 'yellow' },
+---     { name = 'panic', from = 'yellow', to = 'red' },
+---     { name = 'calm', from = 'red', to = 'yellow' },
+---     { name = 'clear', from = 'yellow', to = 'green' }
+---   },
+---   callbacks = {
+---     onpanic = function(fsm, event, from, to)
+---       -- Handle panic
+---     end,
+---   }
+--- }
 function machine.create(options)
     assert(options.events)
 
@@ -227,25 +296,38 @@ function machine.create(options)
     return fsm
 end
 
+--- Seals the state machine, preventing any further modifications.
 function machine:seal()
     self._sealed = true
     self._callbacks = nil
 end
 
+---Checks if the current state is the given state
+---@param state string The state to check against
+---@return boolean True if the current state matches
 function machine:is(state)
     return self._state.current == state
 end
 
+--- Checks if an event can be triggered in the current state.
+--- @param e string The name of the event.
+--- @return boolean, string True if the event can be triggered, Next state if it can
 function machine:can(e)
     local event = self.events[e]
     local to = event and event.map[self._state.current] or event.map['*']
     return to ~= nil, to
 end
 
+--- Checks if an event can be triggered in the current state.
+--- @param e string The name of the event.
+--- @return boolean, string True if the event cannot be triggered, Next state if it can
+--- @see Fsm#can
 function machine:cannot(e)
     return not self:can(e)
 end
 
+--- Generates a Graphviz dot file representation of the state machine.
+--- @return string The dot file content as a string.
 function machine:todot()
     local text = {}
     table.insert(text, 'digraph {\n')
@@ -265,13 +347,23 @@ function machine:todot()
     return table.concat(text)
 end
 
+--- Triggers a transition.
+--- If an `onleave<state>` or `onenter<state>` callback returns `ASYNC`, the transition will be paused.
+--- To resume a paused transition, call this method again with the same event name.
+--- @param event string The name of the event to trigger
+--- @return boolean Returns false if the transition is invalid or cancelled
+--- @see Fsm#cancelTransition
 function machine:transition(event)
     if self._state.currentTransitioningEvent == event then
-        return self[self._state.currentTransitioningEvent](self)
+        return self[self._state.currentTransitioningEvent](self) or false
     end
     return false
 end
 
+--- Cancels an ongoing asynchronous transition for the given event.
+--- This will stop a pending transition that is waiting for `fsm:transition()` to be called again
+--- @param event string The name of the event whose transition should be cancelled
+--- @see Fsm#transition
 function machine:cancelTransition(event)
     if self._state.currentTransitioningEvent == event then
         self._state.asyncState = NONE
